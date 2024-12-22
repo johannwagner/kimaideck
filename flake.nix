61228c7f99dd949e5753d515c10c1a73a0b61730
Johann Wagner <johann.wagner@wobcom.de>
2024-12-22 08:00:39 +0100

    Updated for NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cosmo - ???";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
   outputs = { self, nixpkgs, flake-utils }:
     (flake-utils.lib.eachDefaultSystem (system:
